f80de51670a235f4233ccc38f62b7d881261468f
Wladimir Bandeira <tecnico100@gmail.com>
2022-05-15 16:30:34 -0300

    navegação para detalhes do produto aula - 220

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +18,9 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.deepOrange,
       ),
       home: ProductsOverviewPage(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+      },
       debugShowCheckedModeBanner: false,
     );
   }
