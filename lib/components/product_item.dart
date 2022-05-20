5330be36190b3d892595ffa7ed559707889b74f2
Wladimir Bandeira <tecnico100@gmail.com>
2022-05-20 16:19:40 -0300

    usando Consumer aula 230

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -8,7 +8,10 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(
+        context,
+      listen: false
+    );
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -26,14 +29,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              color: Theme.of(context).accentColor,
             ),
-            color: Theme.of(context).accentColor,
           ),
           title: Text(
             product.title,
